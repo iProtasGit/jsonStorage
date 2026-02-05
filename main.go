48b055676f190451aa9f733389479915efabc29a
iProtas <gerus.kz@gmail.com>
2026-02-05 21:18:09 +0300

    Added bin struct and bin constructor

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+type BinList *[]Bin
+
+type Bin struct {
+	ID        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
+}
+
+func NewBin(id string, private bool, name string, binList BinList) {
+	bin := Bin{
+		ID:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+	*binList = append(*binList, bin)
+}
+
+
+
 func main() {
-    // Your code here
+    binList := BinList(&[]Bin{})
+    NewBin("1", false, "My Bin", binList)
+    fmt.Println(*binList)
 }
