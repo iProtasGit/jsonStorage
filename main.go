886cd0041b0a3cc3afccde082e21ee0bb21e85a1
iProtas <gerus.kz@gmail.com>
2026-02-06 21:52:49 +0300

    Created new packages for different logic

--- main.go
+++ main.go
@@ -2,32 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"jsonStorage/bins"
 )
-
-type BinList *[]Bin
-
-type Bin struct {
-	ID        string
-	Private   bool
-	CreatedAt time.Time
-	Name      string
-}
-
-func NewBin(id string, private bool, name string, binList BinList) {
-	bin := Bin{
-		ID:        id,
-		Private:   private,
-		CreatedAt: time.Now(),
-		Name:      name,
-	}
-	*binList = append(*binList, bin)
-}
-
 
 
 func main() {
-    binList := BinList(&[]Bin{})
-    NewBin("1", false, "My Bin", binList)
+    binList := bins.BinList(&[]bins.Bin{})
+    bins.NewBin("1", false, "My Bin", binList)
     fmt.Println(*binList)
 }
